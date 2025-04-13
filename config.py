d10de9799a1b4ea5153986fe8b232ce3f14a90da
Nikkatsif <nikkatsif@gmail.com>
2025-04-13 15:01:38 +0300

    Removed cache

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 PAGES_LIST = ['Cohere']
 PAGE_CREDENTIALS = {
     "Cohere": {
-        "email": "atsalogria@gmail.com",
-        "password": "Rofl0l0l!10"
+        "email": "",
+        "password": ""
     }
 }
